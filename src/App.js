10cd84a7a7469234768946e9a96a3c031103788b
AlexFisher13 <verji13>
2018-12-07 22:23:16 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { AppHeader } from './components/AppHeader.js'
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
+
+  consider() {
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <div>
+          <AppHeader/>
+          <form>
+          <img src={logo} className="logo" alt="logo" />
+          <TextField type="text" label="Ставка" variant="outlined"
+            id="custom-css-outlined-input"/>
+          <Button type="submit" onClick={this.consider()}
+             variant="contained" color="primary"
+             id="btnCount">Посчитать</Button>
+          </form>
+        </div>
+
+        <div id="result">
+        </div>
+      </section>
     );
   }
 }
