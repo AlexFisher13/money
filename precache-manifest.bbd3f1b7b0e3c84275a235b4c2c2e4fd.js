f82bde4cd00383a03e1ee9235c9389689d6d895a
AlexFisher13 <verji13>
2018-12-10 00:49:36 +0300

    Updates

---
+++ precache-manifest.bbd3f1b7b0e3c84275a235b4c2c2e4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2d7ed2c5c86a501c085b6f14be3728",
+    "url": "/money/static/media/logo.2c2d7ed2.svg"
+  },
+  {
+    "revision": "e587bd2bb2851d1454bd",
+    "url": "/money/static/js/runtime~main.e587bd2b.js"
+  },
+  {
+    "revision": "8ff7729a4814539b1c38",
+    "url": "/money/static/js/main.8ff7729a.chunk.js"
+  },
+  {
+    "revision": "9d6cd6766f480d68a0c6",
+    "url": "/money/static/js/1.9d6cd676.chunk.js"
+  },
+  {
+    "revision": "8ff7729a4814539b1c38",
+    "url": "/money/static/css/main.b808e054.chunk.css"
+  },
+  {
+    "revision": "7769143ba73c1555edb54df61d1b0831",
+    "url": "/money/index.html"
+  }
+];
