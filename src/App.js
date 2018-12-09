b1ce814e9a64e09177decaa66803bc8ff536246f
AlexFisher13 <verji13>
2018-12-10 00:36:04 +0300

    add payment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Button from '@material-ui/core/Button';
 import { AppHeader } from './components/AppHeader.js';
 import TextField from '@material-ui/core/TextField';
-import { AppTable } from './components/AppTable.js';
+import AppTable from './components/AppTable.js';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        resultOpened: false,
+        payment: ''
+      };
+    }
 
-  consider() {
-    
+
+  consider(event) {
+    event.preventDefault(); //Не перегружает страницу при сабмите
+    console.log('Указана ставка ' + this.state.payment)
   }
 
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({payment: event.target.value});
+  }
 
   render() {
     return (
       <section>
         <div>
           <AppHeader/>
-          <form>
-          <img src={logo} className="logo" alt="logo" />
-          <TextField type="text" label="Ставка" variant="outlined"
-            id="custom-css-outlined-input"/>
-          <Button type="submit" onClick={this.consider()}
+          <form onSubmit={this.consider.bind(this)}>
+            <img src={logo} className="logo" alt="logo" />
+            <TextField type="text" label="Ставка" variant="outlined"
+              id="custom-css-outlined-input" onChange={this.handleChange.bind(this)}/>
+
+          <Button type="submit"
              variant="contained" color="primary"
              id="btnCount">Посчитать</Button>
           </form>
         </div>
 
-        <div id="result">
-          <AppTable />
+        <div id="result" className="result">
+          <AppTable payment={this.state.payment} />
         </div>
       </section>
     );
--- src/components/AppTable.js
+++ src/components/AppTable.js
@@ -4,33 +4,48 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import days from '../data/days.js'
 
-export const AppTable = () => {
-     return (
-      <div>
-       <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell numeric>Месяц</TableCell>
-            <TableCell numeric>Количество дней</TableCell>
-            <TableCell numeric>Количество часов</TableCell>
-            <TableCell numeric>Зарплата</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            {
-              days.month.map((month,i) =>
-                <TableRow key={i}>
-                  <TableCell numeric>{days.month[i]}</TableCell>
-                  <TableCell numeric>{days.days[i]}</TableCell>
-                  <TableCell numeric>{days.days[i]*8}</TableCell>
-                  <TableCell numeric>???</TableCell>
-                </TableRow>
-              )
-            }
-        </TableBody>
-      </Table>
-    </div>
-  )
+class AppTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        month: ['Январь', 'Февраль', 'Март',
+        'Апрель', 'Май', 'Июнь', 'Июль', 'Август',
+        'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        days: [17, 19, 20, 21, 20, 20, 22, 23, 20, 23, 21, 21],
+        visible: false
+    }
+  }
+
+
+
+     render() {
+       return (
+        <div>
+         <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell numeric>Месяц</TableCell>
+              <TableCell numeric>Количество дней</TableCell>
+              <TableCell numeric>Количество часов</TableCell>
+              <TableCell numeric>Зарплата</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              {
+                this.state.month.map((month,i) =>
+                  <TableRow key={i}>
+                    <TableCell numeric>{this.state.month[i]}</TableCell>
+                    <TableCell numeric>{this.state.days[i]}</TableCell>
+                    <TableCell numeric>{this.state.days[i]*8}</TableCell>
+                    <TableCell numeric>{this.state.days[i]*8*this.props.payment}</TableCell>
+                  </TableRow>
+                )
+              }
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
 }
+export default AppTable;
